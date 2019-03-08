1a44778c7f26a302d0cb23a59bf9c20281c64e52
wb-lz525541@alibaba-inc.com <wb-lz525541@alibaba-inc.com>
2019-03-08 18:06:25 +0800

    sssss

---
+++ statis.py
@@ -0,0 +1,2 @@
+import subprocess
+print(subprocess.check_output("git log --author='wb-lz525541@alibaba-inc.com' --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf 'added lines: %s, removed lines: %s, total lines: %s\n', add, subs, loc }'"))
